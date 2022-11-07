446952ed7ff9d12993b3956460a0ae02d23a2a46
Bruno Ma <machengcheng2016@gmail.com>
2022-11-07 12:53:16 +0800

    Create base2new_coop_sub.sh

---
+++ CoOp/scripts/base2new_coop_sub.sh
@@ -0,0 +1,23 @@
+cd ..
+
+for dataset in $1
+do
+  for seed in $2
+  do
+    python3 train.py \
+    --root /path/to/data \
+    --seed ${seed} \
+    --trainer CoOp_sub \
+    --dataset-config-file configs/datasets/${dataset}.yaml \
+    --config-file configs/trainers/CoOp_sub/rn50_ep100.yaml \
+    --output-dir output/base2new/train_base/${dataset}/shots_4/CoOp_sub/rn50_ep100/seed${seed} \
+    DIRECT_RESUME output/base2new/train_base/${dataset}/shots_4/CoOp/rn50_ep100/seed${seed}/prompt_learner/model.pth.tar-1 \
+    TRAINER.COOP.N_CTX 16 \
+    TRAINER.COOP.CSC False \
+    TRAINER.COOP.CLASS_TOKEN_POSITION end \
+    DATASET.NUM_SHOTS 4 \
+    TRAIN.CHECKPOINT_FREQ 0 \
+    DATASET.SUBSAMPLE_CLASSES base \
+    TRAINER.U full_P/b2n-${dataset}-CoOp-4shots-nctx16-seed${seed}-start1-finish30-dim10.pth
+  done
+done
