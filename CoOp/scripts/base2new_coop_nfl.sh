d78747935d4099707f25cc670f4f5548920b0cec
Bruno Ma <machengcheng2016@gmail.com>
2022-11-07 12:48:54 +0800

    Create base2new_coop_nfl.sh

---
+++ CoOp/scripts/base2new_coop_nfl.sh
@@ -0,0 +1,23 @@
+cd ..
+
+for dataset in $1
+do
+  for seed in 1 2 3
+  do
+    python3 train.py \
+    --root /path/to/data \
+    --seed ${seed} \
+    --trainer CoOp_nfl \
+    --dataset-config-file configs/datasets/${dataset}.yaml \
+    --config-file configs/trainers/CoOp/rn50_ep100.yaml \
+    --output-dir output/base2new/train_base/${dataset}/shots_4/CoOp/rn50_ep100/seed${seed} \
+    TRAINER.COOP.N_CTX 16 \
+    TRAINER.COOP.CSC False \
+    TRAINER.COOP.CLASS_TOKEN_POSITION end \
+    DATASET.NUM_SHOTS 4 \
+    TRAIN.CHECKPOINT_FREQ 1 \
+    DATASET.SUBSAMPLE_CLASSES base \
+    CLASSNAMES_NFL classnames_nfl/${dataset}.pth \
+    TEXT_FEATURES_NFL text_features_nfl/${dataset}.pth
+  done
+done
