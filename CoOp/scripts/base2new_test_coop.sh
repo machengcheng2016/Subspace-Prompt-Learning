098ba3e55f5ccd0cf6b40552b6cb3b153d15d770
Bruno Ma <machengcheng2016@gmail.com>
2022-11-07 13:00:27 +0800

    Create base2new_test_coop.sh

---
+++ CoOp/scripts/base2new_test_coop.sh
@@ -0,0 +1,25 @@
+cd ..
+
+sub=$1 # [base | new]
+loadep=$2
+for dataset in $3
+do
+  for seed in 1 2 3
+  do
+    python3 train.py \
+    --root /path/to/data \
+    --seed ${seed} \
+    --trainer CoOp \
+    --dataset-config-file configs/datasets/${dataset}.yaml \
+    --config-file configs/trainers/CoOp/rn50_ep100.yaml \
+    --output-dir output/base2new/test_${sub}/${dataset}/shots_4/CoOp/rn50_ep100/seed${seed}/${loadep} \
+    --model-dir output/base2new/train_base/${dataset}/shots_4/CoOp/rn50_ep100/seed${seed} \
+    --load-epoch ${loadep} \
+    --eval-only \
+    TRAINER.COOP.N_CTX 16 \
+    TRAINER.COOP.CSC False \
+    TRAINER.COOP.CLASS_TOKEN_POSITION end \
+    DATASET.NUM_SHOTS 4 \
+    DATASET.SUBSAMPLE_CLASSES ${sub}
+  done
+done
