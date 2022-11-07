0b7923baf42f853a3fb33de0eca00fe8a4a5e848
Bruno Ma <machengcheng2016@gmail.com>
2022-11-07 14:29:34 +0800

    Update train.py

--- CoOp/train.py
+++ CoOp/train.py
@@ -3,7 +3,7 @@ import torch
 import time
 import os
 
-from dassl.utils import setup_logger, set_random_seed, collect_env_info
+from dassl.utils import setup_logger, set_random_seed
 from dassl.config import get_cfg_default
 from dassl.engine import build_trainer
 
